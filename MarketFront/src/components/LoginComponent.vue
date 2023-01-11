08e1a275b68af7882a49f8aab57ff85ada183f15
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2023-01-11 18:15:23 +0000

    LoginComponent template

---
+++ MarketFront/src/components/LoginComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <form >
+      <label>Email</label>
+      <input type="email" />
+      <label>Password</label>
+      <input type="password" />
+      <button type="submit">Login</button>
+    </form>
+  </template>
+
+
