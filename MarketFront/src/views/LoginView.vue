4a91d7cce5e630824e182c6e2d4c250c08226e67
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2023-01-11 18:16:16 +0000

    LoginVIew setup

---
+++ MarketFront/src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<template>
+  <Login />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import Login from '../components/LoginComponent.vue';
+import { useUserStore } from "../stores/user";
+
+export default defineComponent({
+
+  name: 'LoginView',
+
+  components: {
+    Login,
+  },
+
+  setup() {
+      const userStore = useUserStore();
+      return { userStore };
+  },
+
+  data() {
+      return {
+          email: "",
+          password: "",
+      };
+  },
+
+  methods: {
+      async login(){
+          await this.userStore.signIn(this.email, this.password); 
+      },
+  },
+});
+</script>
