e392cfc5baff929817176e24d12641584c0a7c88
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2023-01-11 18:11:48 +0000

    Navbar added to App.vue

--- MarketFront/src/App.vue
+++ MarketFront/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <v-app>
     <v-main>
+      <NavbarComponent/>
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import NavbarComponent from './components/NavbarComponent.vue'
 export default {
   name: 'App',
+
+  components:{
+    NavbarComponent
+  },
 
   data: () => ({
     //
   }),
 }
 </script>
+
+<style>
+
+</style>
